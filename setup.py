23da14941822bd7338d0ff732d4edb12a1a67121
Bisma Shafiq <bismashafiq26@gmail.com>
2024-11-11 13:10:15 +0500

    add first setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+# HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip()]  # Strip newline and remove empty strings
+        # if HYPEN_E_DOT in requirements
+                # requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='data-science',
+    version='0.0.1',
+    author='Bisma',
+    author_email='shafiqbisma648@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirement.txt')
+)
