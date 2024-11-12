5684ca2147385368d979b6512649b227a2e63c3b
Bisma Shafiq <bismashafiq26@gmail.com>
2024-11-12 12:54:56 +0500

    log file cretion

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import logging
-from src.datascienceproject.logger import logging
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), "src"))
+
+from datascience.logger import logging
+
+if __name__=='__main__':
+    logging.info("Execution started")
 
 
-if __name__ == '__main__':
-    logging.info("my first logging file")
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name='datascienceproject',
+    name='DS_Project',
     version='0.0.1',
     author='Bisma',
     author_email='shafiqbisma648@gmail.com',
