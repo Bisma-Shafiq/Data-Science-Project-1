1ac28bb3cd5ae8f413a8b02ca04dbff73122ee5e
Bisma Shafiq <bismashafiq26@gmail.com>
2024-11-12 14:08:48 +0500

    commit

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ import os
 sys.path.append(os.path.join(os.getcwd(), "src"))
 
 from datascience.logger import logging
+from datascience.exception_handling import CustomException
 
 if __name__=='__main__':
-    logging.info("Execution started")
+    logging.info("Execution")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Excetional_handling")
+        raise CustomException(e,sys)
+
 
 
